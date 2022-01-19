03826bb46682bdd1543ec717ff129ba564b93d4c
Guillian Prince <guillianprince@R2Hs-MacBook-Pro.local>
2022-01-18 20:43:03 -0500

    medium challenge

--- medium.js
+++ medium.js
@@ -1,4 +1,52 @@
 //MEDIUM: Create a program that accepts a number (1-12) as input and logs to the console that number and its
 //corresponding month. For example: if the user enters the number 3, then it should return the month “March.” Alert the
 //user if they enter an invalid number (e.g. less than 1 or greater than 12).
+const userInput = prompt("PLEASE ENTER A NUMBER 1-12");
 
+if(userInput == 1){
+    console.log("JANUARY")
+} 
+
+if(userInput == 2){
+    console.log("FEBRUARY")
+} 
+
+if(userInput == 3){
+    console.log("MARCH")
+} 
+
+ if(userInput == 4){
+     console.log("APRIL")
+ } 
+
+ if(userInput == 5){
+     console.log("MAY")
+ } 
+
+ if(userInput == 6){
+     console.log("JUNE")
+ } 
+
+ if(userInput == 7){
+     console.log("JULY")
+ } 
+
+ if(userInput == 8){
+     console.log("AUGUST")
+ } 
+
+ if(userInput == 9){
+     console.log("SEPTEMBER")
+ } 
+
+ if(userInput == 10){
+     console.log("OCTOBER")
+ } 
+
+ if(userInput == 11){
+     console.log("NOVEMBER")
+ } 
+
+ if(userInput == 12){
+     console.log("DECEMBER")
+ } 
