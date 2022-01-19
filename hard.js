884ae37acb52e9a417db36ae4812392d31dd5b65
Guillian Prince <guillianprince@R2Hs-MacBook-Pro.local>
2022-01-18 22:53:36 -0500

    hard challenge

--- hard.js
+++ hard.js
@@ -8,3 +8,25 @@
 
 // Store Tom & Jerry’s mass and height as variables. Calculate both their BMI’s. Create a Boolean variable containing information about whether Tom has a higher BMI than Jerry. Print a string to the console with the variable from step 3 ( e.g. Is Tom’s BMI higher than Jerry’s? false).
 
+var tomHeight = 9
+var tomMass = 8
+
+var jerryHeight = 10
+var jerryMass = 45
+
+var tomBMI = (tomMass / (tomHeight * tomHeight))
+var jerryBMI = (jerryMass / (jerryHeight * jerryHeight))
+
+console.log(tomBMI);
+console.log(jerryBMI);
+
+var compareBMI = tomBMI < jerryBMI 
+
+if(tomBMI < jerryBMI, true){
+    console.log('Jerry\'s BMI is higher than Tom\'s');
+  }
+  else {
+    console.log('Tom\'s BMI is higher than Jerry\'s');
+  }
+
+
